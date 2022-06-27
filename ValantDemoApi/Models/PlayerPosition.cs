313bfebdafb9b2c27369ce99851430bc5147bd63
Minhcaron <minhnhatbui@gmail.com>
2022-06-27 04:36:57 -0400

    Add files via upload

---
+++ ValantDemoApi/Models/PlayerPosition.cs
@@ -0,0 +1,13 @@
+namespace ValantDemoApi {
+    public class PlayerPosition {
+        public int x = 0;
+		public int y = 0;
+		public string name = "bob";
+		
+		public PlayerPosition()
+		{
+			x = 0;
+			y = 0;
+		}
+    }
+}
